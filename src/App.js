4a1e9fa92d4c9c59cb87847ae543288f723610a4
Kountret <kospyrou@megaventory.com>
2022-02-22 19:35:24 +0200

    Retrieve dummy data from JSON placeholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox'
-import { robots } from "./robots";
 import './App.css';
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                robots: result
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          );
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value});
