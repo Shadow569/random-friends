593ea2c1af5e6f5851cb5cdbca3e0c64e1d53c26
Kountret <kospyrou@megaventory.com>
2022-02-23 21:44:19 +0200

    Convert App to functional component with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox'
 import Scroll from "../components/Scroll";
@@ -6,50 +6,44 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 import { ApiConfig } from "../ApiConfig";
 
-class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+const App = () => {
+    const [friends, setFriends] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
-    componentDidMount() {
+
+    const filtered = friends.filter(user => {
+        return user.name.toLowerCase().startsWith(searchfield.toLowerCase());
+    });
+
+    useEffect(() => {
         const url = ApiConfig.apiurl + ApiConfig.retrievalendpoint; //create an ApiConfig.js inside the src folder and populate it with your credentials and pass them along to the actual parameters required by your api provider to make the request
         fetch(url)
           .then(response => response.json())
           .then(
             (userList) => {
-              this.setState({
-                robots: userList
-              });
+             setFriends(userList);
             }
           );
-    }
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
-    render(){
-        const { robots, searchfield } = this.state;
-        const filteredMalakes = robots.filter(user => {
-            return user.name.toLowerCase().startsWith(searchfield.toLowerCase());
-        });
-        return !robots.length ?
-            (
-                <h1 className="tc">Please wait..</h1>
-            ) :
-            (
-                <div className="tc">
-                    <h1 className="f2"><a href='http://localhost:3000/'>My Random Friends</a></h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredMalakes} />
-                        </ErrorBoundary>
-                    </Scroll>    
-                </div>    
-            )
-    }
+    })
+    
+    return !friends.length ?
+        (
+            <h1 className="tc">Please wait..</h1>
+        ) :
+        (
+            <div className="tc">
+                <h1 className="f2"><a href='http://localhost:3000/'>My Random Friends</a></h1>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots={filtered} />
+                    </ErrorBoundary>
+                </Scroll>    
+            </div>    
+        )
 }
 
 export default App;
